0e073989e20d9df02dd266d5cec990d6d6dc71f5
trbk07 <trbaokien@gmail.com>
2026-04-12 10:42:47 +0700

    Cấu trúc lại dự án sau khi xóa những file không cần thiết

--- src/main/java/com/auction/ui/AppLauncher.java
+++ src/main/java/com/auction/ui/AppLauncher.java
@@ -1,7 +1,7 @@
 package com.auction.ui;
 
 public class AppLauncher {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MainApp.main(args);
     }
 }
--- src/main/java/com/auction/ui/MainApp.java
+++ src/main/java/com/auction/ui/MainApp.java
@@ -4,24 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 
 public class MainApp extends Application {
     @Override
-    public void start(Stage stage) throws IOException{
-        //Kiểm tra xem nó có tìm thấy file không
+    public void start(Stage stage) throws IOException {
+        // Kiểm tra xem nó có tìm thấy file không
         URL fxmllocation = getClass().getResource("/fxml/Login.fxml");
         System.out.println("Mày dò máy dò hihi: " + fxmllocation);
-      // Tìm đường dẫn :resources/fxml/Login.fxml
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/Login.fxml"));
+
+        // Nạp file FXML
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("HỆ THỐNG ĐẤU GIÁ - ĐĂNG NHẬP");
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args){
-        launch();
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
