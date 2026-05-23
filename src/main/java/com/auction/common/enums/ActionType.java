fa246aa219d007eba3194f5f709a273fe5a5aca6
Nguyen Hiu <ngxhieu712@gmail.com>
2026-05-23 23:12:37 +0700

    làm lại duyệt admin, demo tạo phiên tạm thời không cần admin duyệt

--- src/main/java/com/auction/common/enums/ActionType.java
+++ src/main/java/com/auction/common/enums/ActionType.java
@@ -22,10 +22,12 @@ public class ActionType {
     public static final String GET_ALL_AUCTIONS = "AUCTION_GET_ALL";
     public static final String GET_JOINED_AUCTIONS = "AUCTION_GET_JOINED";
     public static final String GET_AUCTION_BY_ID = "AUCTION_GET_BY_ID";
-    public static final String JOIN_AUCTION = "AUCTION_JOIN";
-    public static final String LEAVE_AUCTION = "AUCTION_LEAVE";
-    public static final String CLOSE_AUCTION = "AUCTION_CLOSE";
-    public static final String PLACE_BID = "AUCTION_BID_PLACE";
+    public static final String JOIN_AUCTION      = "AUCTION_JOIN";
+    public static final String LEAVE_AUCTION     = "AUCTION_LEAVE";
+    public static final String CLOSE_AUCTION     = "AUCTION_CLOSE";
+    public static final String PLACE_BID         = "AUCTION_BID_PLACE";
+    public static final String CONFIRM_BUY_NOW   = "AUCTION_BUY_NOW_CONFIRM";
+    public static final String SETTLE_BUY_NOW    = "AUCTION_BUY_NOW_SETTLE";
     public static final String REGISTER_AUTO_BID = "AUCTION_REGISTER_AUTO_BID";
     public static final String GET_BID_HISTORY = "AUCTION_BID_HISTORY";
     public static final String GET_DASHBOARD_STATS = "USER_DASHBOARD_STATS";
@@ -37,6 +39,7 @@ public class ActionType {
 
     // --- CHAT REAL-TIME ---
     public static final String SEND_CHAT_MESSAGE = "CHAT_SEND_MESSAGE";
+    public static final String GET_SYSTEM_NOTIFICATIONS = "CHAT_GET_SYSTEM_NOTIFICATIONS";
 
     // --- SERVER PUSH (server chủ động gửi, không có requestId) ---
     public static final String AUCTION_BID_UPDATE = "AUCTION_BID_UPDATE";
